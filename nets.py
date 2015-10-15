0266224e5dbe751597af3f450865b0b3eb615fbf
Dan O'Huiginn <daniel@ohuiginn.net>
2015-10-15 16:41:25 +0200

    basic working version for BP

---
+++ nets.py
@@ -0,0 +1,76 @@
+import nltk
+import string
+from nltk.corpus import stopwords
+import itertools
+import csv
+import glob
+import os
+
+## need to download within the nltk.download window:
+# models/punkt
+# corpora/stopwords
+
+class NgramGenerator(object):
+
+    def __init__(self, *args, **kwargs):
+        self.ngram_dict = {}
+        self.doc_dict = {}
+        self.ngram_ids, self.doc_ids = itertools.count(1), itertools.count(1)
+
+    def iterate_files(self):
+        # (filenames)
+        return glob.glob(self.input_filepattern)
+
+    def ngrams_from_file(self, filename):
+        text = open(filename).read()
+        text = text.lower().translate(None, string.punctuation)
+        words = nltk.word_tokenize(text)
+        stops = stopwords.words('english')
+        words = [x for x in words if x not in stops]
+        trigrams = nltk.trigrams(words)
+        return trigrams
+
+    def add_ngrams(self, trigrams):
+        with open(self.fn_ngrams, 'a+') as fh:
+            dest = csv.writer(fh)
+            dest.writerows(trigrams)        
+
+    def final_output(self):
+        with open(self.fn_docdict, 'w') as fh:
+            dest = csv.writer(fh)
+            dest.writerows(self.doc_dict.iteritems())        
+        with open(self.fn_ngramdict, 'w') as fh:
+            dest = csv.writer(fh)
+            ngrams = sorted((y, ' '.join(x)) for (x,y) in self.ngram_dict.iteritems())
+            dest.writerows(ngrams)        
+                
+    def run(self):
+        if os.path.exists(self.fn_ngrams):
+            os.unlink(self.fn_ngrams)
+        
+        for fn in self.iterate_files()[:25]:
+            filenum = next(self.doc_ids)
+            self.doc_dict[filenum] = fn
+
+            new_ngrams = []
+            for trigram in self.ngrams_from_file(fn):
+                if trigram in self.ngram_dict:
+                    tg_num = self.ngram_dict[trigram]
+                else:
+                    tg_num = next(self.ngram_ids)
+                    self.ngram_dict[trigram] = tg_num
+                new_ngrams.append([filenum, tg_num])
+            self.add_ngrams(new_ngrams)            
+        self.final_output()
+
+        
+class BPNGrams(NgramGenerator):
+    fn_ngramdict = '/tmp/bp_ngram_ids.csv'
+    fn_docdict = '/tmp/bp_doc_ids.csv'
+    fn_ngrams = '/tmp/bp_ngrams.csv'
+
+    input_filepattern = '/home/src/oonetworks/data/bp/*txt'
+
+
+        
+
