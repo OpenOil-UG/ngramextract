95cf8dc12de25733a251ef6a1bb0a67c6cfc7e75
Dan O'Huiginn <daniel@ohuiginn.net>
2015-10-23 12:38:52 +0200

    Output pairs of ngrams which co-occur

--- find_pairs.py
+++ find_pairs.py
@@ -24,6 +24,13 @@ import csv
 import pandas as pd
 import numpy as np
 from collections import defaultdict
+from itertools import chain, islice
+
+def ichunked(seq, chunksize):
+    """Yields items from an iterator in iterable chunks."""
+    it = iter(seq)
+    while True:
+        yield chain([next(it)], islice(it, chunksize-1))
 
 class PairBuilder(object):
 
@@ -34,7 +41,26 @@ class PairBuilder(object):
     def run(self):
         indata = pd.read_csv(self.input_fn, names = ['doc_id', 'ngram_id', 'position'])
         outdata = defaultdict(list)
-        return indata
+        sliding_window = ichunked(indata.iterrows(), self.threshold_distance)
+        for window in sliding_window:
+            # each of these is 10-line segment of the input window
+            first = next(window)[1]
+            for dist, row in window:
+                if row.doc_id != first.doc_id:
+                    print('skipping item in different doc')
+                    continue
+                outdata[(first.ngram_id, row.ngram_id)].append(row.position - first.position)
+        self.write_csv(outdata)
+        return indata, outdata
+
+    def write_csv(self, outdata):
+        with open(self.output_fn, 'w') as outf:
+            writer = csv.writer(outf)
+            for k,v in outdata.items():
+                if len(v) > 1:
+                    row = list(k) + v
+                    writer.writerow(row)
+
         
         
         
